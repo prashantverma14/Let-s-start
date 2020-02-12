636e2e24ceb6fadcabd35083050b1b20e72249ed
Prashant Verma <prashant.verma_cs19@gla.ac.in>
2020-02-12 11:43:54 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,7 @@
+i=0
+j=0
+for i in range(5,0,-1):
+	for j in range(i):
+		print("*",end=" ")
+
+	print()
---
+++ pattern3.py
@@ -0,0 +1,4 @@
+x=int(input("enter the no:"))
+for i in range(1,x+1):
+	print(" "*(x-i) + "*"*i)
+
